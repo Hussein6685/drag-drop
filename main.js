deab99b0e83f7fafae1fa889d2583a8e1c8d4c03
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2023-10-06 18:44:45 +0300

    all end

---
+++ main.js
@@ -0,0 +1,50 @@
+let btn = document.getElementById('btn');
+let inp = document.getElementById('inp');
+let boxes = document.querySelectorAll('.box');
+let drag = null;
+
+btn.onclick = function (){
+    if(inp.value != ""){
+        boxes[0].innerHTML += `
+        <p class="item" draggable ="true">${inp.value}</p>
+        `
+        inp.value = "";
+    }
+
+    dragItem();
+
+}
+function dragItem() {
+    let items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        item.addEventListener('dragstart', function(){
+            // console.log('drag start')
+            drag = item;
+            item.style.opacity = '0.5';
+        })
+
+        item.addEventListener('dragend', function(){
+            // console.log('drag end')
+            drag = null;
+            item.style.opacity = '1';
+        })
+
+        boxes.forEach(box => {
+            box.addEventListener('dragover', function(e) {
+                e.preventDefault()
+                // console.log('darg')
+                this.style.background = '#090';
+                this.style.color = '#fff';
+            })
+            box.addEventListener('dragleave', function () {
+                this.style.background = '#fff';
+                this.style.color = '#000';
+            })
+            box.addEventListener('drop', function () {
+                box.append(drag);
+                this.style.background = '#fff';
+                this.style.color = '#000';
+            })
+        })
+    })
+}
